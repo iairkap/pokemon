72f7641b80031d72356d8ed9aeb047ac766e73ac
iairkap <iairkap@gmail.com>
2023-05-15 14:08:40 -0300

    front

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -2,16 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { setPokemons } from "../../redux/actions/actions";
+import styles from "./Card.module.css";
 
 function Card({ id, image, name, types }) {
   const dispatch = useDispatch();
 
   return (
-    <div className="Card">
-      <Link to={`/pokemon/${id}`} onClick={() => dispatch(setPokemons())}>
-        <img src={image} alt={name} />
+    <div className={styles.card}>
+      <Link to={`/detail/${id}`} onClick={() => dispatch(setPokemons())}>
+        <img className={styles.imagen} src={image} alt={name} />
       </Link>
-      <div className="Card__info">
+      <div className={styles.card_info}>
         <h3 className="Name">{name}</h3>
         <div className="Types">
           {types?.length > 0 &&
