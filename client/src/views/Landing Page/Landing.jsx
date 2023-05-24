758801fc1042f555f6ef36d552264c84f08a315d
iairkap <iairkap@gmail.com>
2023-05-24 16:30:27 -0300

    finito2.0

--- client/src/views/Landing Page/Landing.jsx
+++ client/src/views/Landing Page/Landing.jsx
@@ -1,46 +1,51 @@
+/*
+ */
+
 import React from "react";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
 import { getPokemons, getType } from "../../redux/actions/actions";
 import Pokemonlogo from "../../assets/PokemonLogo.jsx";
 import styles from "./Landing.module.css";
 import charizardGif from "../../assets/charizard.gif";
+import { connect } from "react-redux";
 
-function Landing() {
-  const dispatch = useDispatch();
-  const pokemons = useSelector((state) => state.pokemons);
-  const types = useSelector((state) => state.types);
+class Landing extends React.Component {
+  componentDidMount() {
+    const { dispatch, types, pokemons } = this.props;
 
-  useEffect(() => {
     if (types.length === 0 && pokemons.length === 0) {
       dispatch(getType());
       dispatch(getPokemons());
     }
-  }, []);
+  }
 
-  return (
-    <div>
-      <div className={styles.Landing}>
-        <div className={styles.LandingContainer}>
-          <img
-            src={charizardGif}
-            alt="Charizard"
-            className={styles.charizardGif}
-          />
-          <div className={styles.logoContainer}>
-            {" "}
-            <Pokemonlogo />
-          </div>
-          <div className={styles.LandingBotonContainer}>
-            <Link to="/home">
-              <button className={styles.botonHome}>Atrapalos Ya!</button>
-            </Link>
+  render() {
+    return (
+      <div>
+        <div className={styles.Landing}>
+          <div className={styles.LandingContainer}>
+            <img
+              src={charizardGif}
+              alt="Charizard"
+              className={styles.charizardGif}
+            />
+            <div className={styles.logoContainer}>
+              <Pokemonlogo />
+            </div>
+            <div className={styles.LandingBotonContainer}>
+              <Link to="/home">
+                <button className={styles.botonHome}>Atrapalos Ya!</button>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
-export default Landing;
+export default connect((state) => ({
+  types: state.types,
+  pokemons: state.pokemons,
+}))(Landing);
